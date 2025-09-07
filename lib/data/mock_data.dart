21913dd860270392078861fd7f0a90e841c5f790
dmakarau <denis.makarow@gmail.com>
2025-09-07 19:16:31 +0200

    Added model and mock data, based on the model

---
+++ lib/data/mock_data.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import 'package:meals/models/category.dart';
+
+const extraCategories = [
+  Category(
+    id: 'c11',
+    title: 'Mediterranean',
+    color: Colors.indigo,
+  ),
+  Category(
+    id: 'c12',
+    title: 'Street Food',
+    color: Colors.deepOrange,
+  ),
+  Category(
+    id: 'c13',
+    title: 'Desserts',
+    color: Colors.pinkAccent,
+  ),
+  Category(
+    id: 'c14',
+    title: 'Vegan & Healthy',
+    color: Colors.lightGreen,
+  ),
+  Category(
+    id: 'c15',
+    title: 'BBQ & Grilled',
+    color: Colors.brown,
+  ),
+  Category(
+    id: 'c16',
+    title: 'Seafood',
+    color: Colors.cyan,
+  ),
+  Category(
+    id: 'c17',
+    title: 'Soups & Stews',
+    color: Colors.deepPurple,
+  ),
+  Category(
+    id: 'c18',
+    title: 'Latin American',
+    color: Colors.orangeAccent,
+  ),
+  Category(
+    id: 'c19',
+    title: 'Festive Specials',
+    color: Colors.redAccent,
+  ),
+  Category(
+    id: 'c20',
+    title: 'Snacks & Appetizers',
+    color: Colors.blueGrey,
+  ),
+];
