cfea035120c0b70f25e4212ba445a2d412278a11
dmakarau <denis.makarow@gmail.com>
2025-09-27 21:48:16 +0200

    Added side drawer

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:meals/models/meals.dart';
 import 'package:meals/screens/categories_screen.dart';
 import 'package:meals/screens/meals_screen.dart';
+import 'package:meals/widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
@@ -19,10 +20,7 @@ class _TabsScreenState extends State<TabsScreen> {
   void showInfoMessage(String message) {
     ScaffoldMessenger.of(context).clearSnackBars();
     ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        duration: const Duration(seconds: 2),
-        content: Text(message),
-      ),
+      SnackBar(duration: const Duration(seconds: 2), content: Text(message)),
     );
   }
 
@@ -30,7 +28,7 @@ class _TabsScreenState extends State<TabsScreen> {
     final isExisting = _favoriteMeals.contains(meal);
     if (isExisting) {
       setState(() {
-        _favoriteMeals.remove(meal);  
+        _favoriteMeals.remove(meal);
       });
       showInfoMessage('Meal removed from favorites.');
     } else {
@@ -41,15 +39,27 @@ class _TabsScreenState extends State<TabsScreen> {
     }
   }
 
-  void selectPage(int index) {
+  void _selectPage(int index) {
     setState(() {
       _selectedTabIndex = index;
     });
   }
 
+  void _setScreen(String identifier) {
+    if (identifier == 'meals') {
+      Navigator.of(context).pop();
+     
+     
+    } else if (identifier == 'filters') {
+      // TODO: Navigate to filters screen
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    Widget activeTab = CategoriesScreen(onToogleFavorite: toogleMealFavoriteStatus);
+    Widget activeTab = CategoriesScreen(
+      onToogleFavorite: toogleMealFavoriteStatus,
+    );
     String activeTabTitle = 'Categories';
 
     if (_selectedTabIndex == 1) {
@@ -62,10 +72,11 @@ class _TabsScreenState extends State<TabsScreen> {
 
     return Scaffold(
       appBar: AppBar(title: Text(activeTabTitle)),
+      drawer: MainDrawer(onSelectScreen: _setScreen),
       body: activeTab,
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedTabIndex,
-        onTap: selectPage,
+        onTap: _selectPage,
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.set_meal),
