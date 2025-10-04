76a87902bb75f1dee99ba660e5cb96535d061070
dmakarau <denis.makarow@gmail.com>
2025-10-04 10:46:48 +0200

    fixed tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,10 +7,18 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'package:meals/main.dart';
 
 void main() {
+  setUpAll(() {
+    // Prevent google_fonts from trying to fetch fonts over the network during tests.
+    // Some google_fonts versions expose a config setter; if not, assigning a new
+    // config object may be required. This line is the simplest common form.
+    GoogleFonts.config.allowRuntimeFetching = false;
+  });
+
   testWidgets('App starts and displays tab screen', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const App());
